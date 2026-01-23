7c98c15a299a02d3d41024a768bc97cfbd03c5c5
Rohan Munot <rohanmunot24@gmail.com>
2026-01-23 18:20:31 +0530

    css update

--- components/badge.tsx
+++ components/badge.tsx
@@ -16,7 +16,7 @@ const Badge = ({ render, className, ...props }: BadgeProps) => {
           "[&_svg]:size-4 sm:[&_svg]:size-3.5 flex gap-2 items-center",
           "px-2 py-0.5 text-xs rounded-sm bg-muted text-muted-foreground relative border border-border/50 shadow-lg",
           "before:pointer-events-none before:absolute before:inset-0 before:rounded-[calc(var(--radius-sm)-0.5px)]",
-          "before:shadow-[0_-1px_0_0_rgba(0,0,0,0.2)] dark:before:shadow-[0_-1px_0px_0_rgba(255,255,255,0.2)]",
+          "before:shadow-[0_-1px_0_0_oklch(0.45_0.07_55/0.25)] dark:before:shadow-[0_-1px_0px_0_oklch(0.85_0.04_70/0.25)]",
           className,
         ),
       },
