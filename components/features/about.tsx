15ab7cd5159d3ab04b854d7917bc7c84d0c8da44
Rohan Munot <rohanmunot24@gmail.com>
2026-06-05 02:16:12 +0530

    fix: gap

--- components/features/about.tsx
+++ components/features/about.tsx
@@ -32,7 +32,7 @@ const About = () => {
           </span>
         </div>
 
-        <div className="flex flex-wrap gap-2">
+        <div className="flex flex-wrap gap-2 gap-y-0">
           <span className="text-[11px] text-muted-foreground mt-0.5">
             ~
           </span>
