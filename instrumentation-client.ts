0c67b4a06b79926c90d0a942c188de6469a3e15b
Rohan Munot <rohanmunot24@gmail.com>
2026-06-07 03:20:37 +0530

    feat: posthog added

---
+++ instrumentation-client.ts
@@ -0,0 +1,7 @@
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: '2026-01-30',
+  cookieless_mode: "always",
+})
